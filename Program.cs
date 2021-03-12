16b66ab826ff93cd79387b70e57c7a9a16bbc8ae
Hasif Priyambudi <hasifpriyambudi13@gmail.com>
2021-03-12 12:28:38 +0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace calculator{
+    class Program{
+        static void Main(string[] args){
+            Console.Title = "Aplikasi Kalkulator!";
+            Console.Write("Inputkan Nilai A = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Inputkan Nilai B = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hasil Penambahan "+a+"+"+b+" = "+Penambahan(a,b));
+            Console.WriteLine("Hasil Pengurangan "+a+"-"+b+" = "+Pengurangan(a,b));
+            Console.WriteLine("Hasil Perkalian "+a+"*"+b+" = "+Perkalian(a,b));
+            Console.WriteLine("Hasil Pembagian "+a+"/"+b+" = "+Pembagian(a,b));
+        }
+        
+        static int Penambahan(int a, int b){
+            return a+b;
+        }
+
+        static int Pengurangan(int a, int b){
+            return a-b;
+        }
+
+        static int Pembagian(int a, int b){
+            return a/b;
+        }
+
+        static int Perkalian(int a, int b){
+            return a*b;
+        }
+    }
+}
